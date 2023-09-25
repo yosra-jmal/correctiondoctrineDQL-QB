95ac924e4b819a9ffb375376e30e3e994d34cbc3
unknown <mehdy.benromdhane@esprit.tn>
2023-09-25 14:56:57 +0100

    Définir et réutiliser le tableau des auteurs comme propriété de classe

--- src/Controller/AuthorController.php
+++ src/Controller/AuthorController.php
@@ -8,6 +8,24 @@ use Symfony\Component\Routing\Annotation\Route;
 
 class AuthorController extends AbstractController
 {
+    public $authors = array(
+
+        array(
+            'id' => 1, 'picture' => '/images/Victor-Hugo.jpg',
+            'username' => ' Victor Hugo', 'email' => 'victor.hugo@gmail.com ', 'nb_books' => 100
+        ),
+        array(
+            'id' => 2, 'picture' => '/images/william-shakespeare.jpg',
+            'username' => ' William Shakespeare', 'email' => ' william.shakespeare@gmail.com', 'nb_books' => 200
+        ),
+        array(
+            'id' => 3, 'picture' => '/images/Taha_Hussein.jpg',
+            'username' => ' Taha Hussein', 'email' => 'taha.hussein@gmail.com', 'nb_books' => 300
+        ),
+    );
+
+
+
     #[Route('/author', name: 'app_author')]
     public function index(): Response
     {
@@ -31,25 +49,8 @@ class AuthorController extends AbstractController
     #[Route('/list', name: 'list_author')]
     public function list()
     {
-
-        $authors = array(
-
-            array(
-                'id' => 1, 'picture' => '/images/Victor-Hugo.jpg',
-                'username' => ' Victor Hugo', 'email' => 'victor.hugo@gmail.com ', 'nb_books' => 100
-            ),
-            array(
-                'id' => 2, 'picture' => '/images/william-shakespeare.jpg',
-                'username' => ' William Shakespeare', 'email' => ' william.shakespeare@gmail.com', 'nb_books' => 200
-            ),
-            array(
-                'id' => 3, 'picture' => '/images/Taha_Hussein.jpg',
-                'username' => ' Taha Hussein', 'email' => 'taha.hussein@gmail.com', 'nb_books' => 300
-            ),
-        );
-
         return $this->render('author/list.html.twig', [
-            'authors' => $authors,
+            'authors' => $this->authors,
         ]);
     }
 
@@ -57,24 +58,8 @@ class AuthorController extends AbstractController
     public function authorDetails($id)
     {
         $author = null;
-        $authors = array(
-
-            array(
-                'id' => 1, 'picture' => '/images/Victor-Hugo.jpg',
-                'username' => ' Victor Hugo', 'email' => 'victor.hugo@gmail.com ', 'nb_books' => 100
-            ),
-            array(
-                'id' => 2, 'picture' => '/images/william-shakespeare.jpg',
-                'username' => ' William Shakespeare', 'email' => ' william.shakespeare@gmail.com', 'nb_books' => 200
-            ),
-            array(
-                'id' => 3, 'picture' => '/images/Taha_Hussein.jpg',
-                'username' => ' Taha Hussein', 'email' => 'taha.hussein@gmail.com', 'nb_books' => 300
-            ),
-        );
-
         // Parcourez le tableau pour trouver l'auteur correspondant à l'ID
-        foreach ($authors as $authorData) {
+        foreach ($this->authors as $authorData) {
             if ($authorData['id'] == $id) {
                 $author = $authorData;
             };
